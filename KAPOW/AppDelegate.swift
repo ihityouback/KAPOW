aa82475f83d6e125f9823b04abdd8e7e9002938f
Rustam Galiullin <ihityouback@Rustams-MacBook-Pro.local>
2016-09-03 20:23:45 +0400

    Project initial commit

---
+++ KAPOW/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  KAPOW
+//
+//  Created by Rustam Galiullin on 03/09/16.
+//  Copyright © 2016 Rustam Galiullin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {}
+}
+
