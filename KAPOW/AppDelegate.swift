32c25cf868575aa3ccdc824ab5bf827d64b03adb
Rustam Galiullin <formifi@list.ru>
2017-11-04 15:23:35 +0400

    Migrated to swift4, fixed image anchor

--- KAPOW/AppDelegate.swift
+++ KAPOW/AppDelegate.swift
@@ -16,14 +16,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var mainWindow: NSWindow?
     let images = ImagesManager()
     
+    let startScale: CGFloat = 0.2
+    let endScale: CGFloat = 4
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         
-        if let window = NSApplication.shared().windows.first {
+        if let window = NSApplication.shared.windows.first {
             mainWindow = window
             window.setFrame(NSRect(x: 0, y: 0, width: 0, height: 0), display: true, animate: false)
             window.isOpaque = false
-            window.level = Int(CGWindowLevelForKey(CGWindowLevelKey.floatingWindow))
-            window.collectionBehavior = .canJoinAllSpaces
+            window.level = NSWindow.Level(rawValue: Int(CGWindowLevelForKey(CGWindowLevelKey.floatingWindow)))
+            window.collectionBehavior = NSWindow.CollectionBehavior.canJoinAllSpaces
         }
         
         configureShortcut()
@@ -32,7 +35,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func configureShortcut() {
         
         guard let shortcut =  MASShortcut(keyCode: UInt(kVK_ANSI_Q),
-                                          modifierFlags: NSEventModifierFlags.command.rawValue) else { return }
+                                          modifierFlags: NSEvent.ModifierFlags.command.rawValue) else { return }
         
         let archivedKey = NSKeyedArchiver.archivedData(withRootObject: shortcut)
         UserDefaults.standard.setValue(archivedKey, forKey: "KAPOWShortcut")
@@ -42,41 +45,50 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         })
     }
     
-    func applicationWillTerminate(_ aNotification: Notification) {}
-    
     func shot () {
         print("Shot!")
         
-        let rect = NSScreen.main()?.frame
-        let window = NSWindow(contentRect: rect!, styleMask: NSBorderlessWindowMask, backing: NSBackingStoreType.buffered, defer: false)
+        guard let rect = NSScreen.main?.frame else { return }
+        let window = NSWindow(contentRect: rect, styleMask: NSWindow.StyleMask.borderless, backing: NSWindow.BackingStoreType.buffered, defer: false)
         window.backgroundColor = NSColor.clear
         window.isOpaque = false
         window.alphaValue = 1
-        window.makeKeyAndOrderFront(NSApplication.shared())
-        window.level = Int(CGWindowLevelForKey(CGWindowLevelKey.floatingWindow))
-        mainWindow?.addChildWindow(window, ordered: NSWindowOrderingMode.above)
+        window.makeKeyAndOrderFront(NSApplication.shared)
+        window.level = NSWindow.Level(rawValue: Int(CGWindowLevelForKey(CGWindowLevelKey.floatingWindow)))
+        mainWindow?.addChildWindow(window, ordered: NSWindow.OrderingMode.above)
         
         let imageView = NSImageView(frame: window.frame)
         imageView.image = images.randomImage()
         imageView.wantsLayer = true
         window.contentView?.addSubview(imageView)
         
-        imageView.layer?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        imageView.frame = rect
+        imageView.superview?.wantsLayer = true
         
         let scaleAnimation = CABasicAnimation(keyPath: "transform.scale")
-        scaleAnimation.fromValue = 0.2
-        scaleAnimation.toValue = 5
-        
+        scaleAnimation.fromValue = startScale
+        scaleAnimation.toValue = endScale
+
         let alphaAnimation = CABasicAnimation(keyPath: "opacity")
         alphaAnimation.fromValue = 1
         alphaAnimation.toValue = 0
         
+        let halfWidth = rect.size.width/2
+        let xAnimation = CABasicAnimation(keyPath: "transform.translation.x")
+        xAnimation.fromValue = halfWidth - halfWidth * startScale
+        xAnimation.toValue = halfWidth - halfWidth * endScale
+        
+        let halfHeight = rect.size.height/2
+        let yAnimation = CABasicAnimation(keyPath: "transform.translation.y")
+        yAnimation.fromValue = halfHeight - halfHeight * startScale
+        yAnimation.toValue = halfHeight - halfHeight * endScale
+
         let animationGroup = CAAnimationGroup()
         animationGroup.duration = 1
         animationGroup.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
         animationGroup.fillMode = kCAFillModeForwards
         animationGroup.isRemovedOnCompletion = false
-        animationGroup.animations = [scaleAnimation, alphaAnimation]
+        animationGroup.animations = [scaleAnimation, alphaAnimation, xAnimation, yAnimation]
         CATransaction.begin()
         CATransaction.setCompletionBlock { [weak self] in
             self?.mainWindow?.removeChildWindow(window)
